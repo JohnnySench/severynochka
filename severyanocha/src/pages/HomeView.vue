ce3be96f5ae315de87d416317646890f709163d8
Jeka <jonrovniy@yandex.ru>
2023-11-14 21:46:29 +0300

    Добавил типографию, и стандартные стили

--- severyanocha/src/pages/HomeView.vue
+++ severyanocha/src/pages/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import Typography from "@/shared/text/ui/Typography.vue";
 </script>
 
 <template>
   <main>
-    <div>Home</div>
+    <Typography :tag-name="'h1'" bold>Hello</Typography>
+    <Typography :tag-name="'h2'" bold>HEllo</Typography>
+    <Typography :tag-name="'h3'" bold>HEllo</Typography>
+    <Typography :tag-name="'h4'" bold>HEllo</Typography>
+    <Typography :tag-name="'h5'" bold>HEllo</Typography>
   </main>
 </template>
---
+++ severyanocha/src/shared/text/ui/Typography.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+interface Props {
+  tagName: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5',
+  bold?: boolean,
+  size?: 'xs' | 'x' | 'm' | 'l' | 'xl'
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <component
+      :is="props.tagName"
+      :class="['typography', size ?`size_${size}`: '', bold ? 'bold' : '', `tagName_${tagName}`]"
+  >
+    <slot/>
+  </component>
+</template>
+
+<style scoped>
+@media screen and (max-width: 768px){
+  .size_xs {
+    font-size: 8px;
+  }
+
+  .size_s {
+    font-size: 12px;
+  }
+
+  .size_m {
+    font-size: 14px;
+  }
+
+  .size_l {
+    font-size: 20px;
+  }
+
+  .size_xl {
+    font-size: 32px;
+  }
+  .tagName_h1 {
+    font-size: 48px;
+  }
+
+  .tagName_h2 {
+    font-size: 36px;
+  }
+
+  .tagName_h3 {
+    font-size: 24px;
+  }
+
+  .tagName_h4 {
+    font-size: 20px;
+  }
+
+  .tagName_h5 {
+    font-size: 16px;
+  }
+}
+
+.typography {
+  line-height: 150%;
+  font-style: normal;
+}
+
+.size_xs {
+  font-size: 12px;
+}
+
+.size_s {
+  font-size: 16px;
+}
+
+.size_m {
+  font-size: 18px;
+}
+
+.size_l {
+  font-size: 24px;
+}
+
+.size_xl {
+  font-size: 36px;
+}
+
+.bold {
+  font-weight: 700;
+}
+
+.tagName_h1 {
+  font-size: 64px;
+}
+
+.tagName_h2 {
+  font-size: 48px;
+}
+
+.tagName_h3 {
+  font-size: 36px;
+}
+
+.tagName_h4 {
+  font-size: 24px;
+}
+
+.tagName_h5 {
+  font-size: 18px;
+}
+
+</style>
